9b01f6090e92bb3da3c6eec9b6e6985afd13322e
Andrea Angelini <andrea-angelini@outlook.it>
2023-07-25 16:27:54 +0200

    threadsafe

--- src/TinyEcs.Core/TypeInfo{T}.cs
+++ src/TinyEcs.Core/TypeInfo{T}.cs
@@ -2,6 +2,7 @@ namespace TinyEcs;
 
 static class TypeInfo<T> where T : unmanaged
 {
+	private static readonly object _lock = new();
 	private static readonly EntitySparseSet<EntityID> _ids = new();
 
 	public static unsafe readonly int Size = sizeof(T);
@@ -10,16 +11,19 @@ static class TypeInfo<T> where T : unmanaged
 	{
 		Debug.Assert(world != null);
 
-		ref var cmpID = ref _ids.Get(world.ID);
-		if (Unsafe.IsNullRef(ref cmpID) || !world.IsAlive(cmpID))
+		lock (_lock)
 		{
-			var ent = world.SpawnEmpty();
-			cmpID = ref _ids.Add(world.ID, ent.ID);
-			world.Set(cmpID, new EcsComponent(cmpID, Size));
-			world.Set<EcsEnabled>(cmpID);
-		}
+			ref var cmpID = ref _ids.Get(world.ID);
+			if (Unsafe.IsNullRef(ref cmpID) || !world.IsAlive(cmpID))
+			{
+				var ent = world.SpawnEmpty();
+				cmpID = ref _ids.Add(world.ID, ent.ID);
+				world.Set(cmpID, new EcsComponent(cmpID, Size));
+				world.Set<EcsEnabled>(cmpID);
+			}
 
-		return cmpID;
+			return cmpID;
+		}
 	}
 }
 
