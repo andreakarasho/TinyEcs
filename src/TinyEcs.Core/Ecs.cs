adc4a665b4e92c5ed98d4e9bd46dd727c739ee9b
Andrea Angelini <andrea-angelini@outlook.it>
2023-07-29 00:30:54 +0200

    iterator file

--- src/TinyEcs.Core/Ecs.cs
+++ src/TinyEcs.Core/Ecs.cs
@@ -223,36 +223,3 @@ public static class SortExtensions
 	}
 }
 #endif
-
-
-
-public delegate void IteratorDelegate(ref Iterator it);
-
-public readonly ref struct Iterator
-{
-	private readonly Archetype _archetype;
-
-	internal Iterator(Commands? commands, Archetype archetype)
-	{
-		Commands = commands;
-		_archetype = archetype;
-	}
-
-	public Commands? Commands { get; }
-	public World World => _archetype.World;
-	public int Count => _archetype.Count;
-	public float DeltaTime => World.DeltaTime;
-
-
-	public readonly Span<T> Field<T>() where T : unmanaged
-		=> _archetype.Field<T>();
-
-	public readonly bool Has<T>() where T : unmanaged
-		=> _archetype.Has<T>();
-
-	public readonly EntityView Entity(int i)
-		=> _archetype.Entity(i);
-
-	internal readonly Span<byte> GetComponentRaw(EntityID id, int row, int count)
-		=> _archetype.GetComponentRaw(id, row, count);
-}
