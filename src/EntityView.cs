5757af685a847326a7059a2b4877382daac0d58b
andreakarasho <andrea-angelini@outlook.it>
2024-03-03 11:17:48 +0100

    gen & id

--- src/EntityView.cs
+++ src/EntityView.cs
@@ -19,6 +19,9 @@ public readonly struct EntityView : IEquatable<EcsID>, IEquatable<EntityView>
         ID = id;
     }
 
+	public EcsID Generation => IDOp.GetGeneration(ID);
+	public EcsID RealID => IDOp.RealID(ID);
+
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly bool Equals(EcsID other) => ID == other;
@@ -26,16 +29,20 @@ public readonly struct EntityView : IEquatable<EcsID>, IEquatable<EntityView>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly bool Equals(EntityView other) => ID == other.ID;
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly override int GetHashCode() => ID.GetHashCode();
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly override bool Equals(object? obj) => obj is EntityView ent && Equals(ent);
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly EntityView Set<T>() where T : struct
 	{
         World.Set<T>(ID);
         return this;
     }
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly EntityView Set<T>(T component) where T : struct
 	{
         World.Set(ID, component);
@@ -62,34 +69,43 @@ public readonly struct EntityView : IEquatable<EcsID>, IEquatable<EntityView>
 	// 	return this;
 	// }
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly EntityView Unset<T>() where T : struct
 	{
         World.Unset<T>(ID);
         return this;
     }
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly EntityView Enable()
     {
         World.Unset<EcsDisabled>(ID);
         return this;
     }
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly EntityView Disable()
     {
         World.Set<EcsDisabled>(ID);
         return this;
     }
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ReadOnlySpan<EcsComponent> Type() => World.GetType(ID);
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ref T Get<T>() where T : struct => ref World.Get<T>(ID);
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly bool Has<T>() where T : struct => World.Has<T>(ID);
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly void Delete() => World.Delete(ID);
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly bool Exists() => World.Exists(ID);
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly bool IsEnabled() => !Has<EcsDisabled>();
 
 
