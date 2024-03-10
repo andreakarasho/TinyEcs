7cd7bd93177b8cd95edc24110856cbb01324724c
andreakarasho <andrea-angelini@outlook.it>
2024-03-10 20:36:06 +0100

    pack

---
+++ scripts/pack.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if the version parameter is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+version="$1"
+
+# Pack main project
+dotnet pack ../src/ -c Release -o ../dist -p:PackageVersion=$version
+
+# Pack TinyEcs.Plugins project
+dotnet pack ../plugins/TinyEcs.Plugins -c Release -o ../dist -p:PackageVersion=$version
