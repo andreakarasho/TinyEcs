1a30d16177f10dcd17d523a9c40d391e2d251782
andreakarasho <andrea-angelini@outlook.it>
2024-09-19 23:54:37 +0200

    + ptr

---
+++ src/Ptr.cs
@@ -0,0 +1,12 @@
+﻿namespace TinyEcs;
+
+public unsafe struct Ptr<T> where T : struct
+{
+	public T* Pointer;
+
+	public ref T Ref
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		get => ref *Pointer;
+	}
+}
